b530ad0505670064eb0311e07e1b9189b8df3f7c
DESKTOP-RBGTKHC\Sido_ <Sido_denis@mail.ru>
2021-03-06 16:44:36 +0300

    commit

---
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -0,0 +1,94 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Message;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+
+@Controller
+public class MainController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    //@Value("${upload.path}")
+    //private String uploadPath;
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
+
+    @Transactional
+    @GetMapping("/main")
+    public String main(@RequestParam(required = false, defaultValue = "") String filter,
+                       @RequestParam(required = false, defaultValue = "") String tag,
+                       Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (tag != null && !tag.isEmpty()) {
+            messageRepo.deleteByTag(tag);
+        }
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+        model.put("filter", filter);
+        return "main";
+    }
+
+    @Transactional
+    @PostMapping("/main")
+    public String add(@AuthenticationPrincipal User user,
+                      @RequestParam String text,
+                      @RequestParam String tag, Map<String, Object> model,
+                      @RequestParam(required = false, defaultValue = "") String filter) throws IOException {
+        Message message = new Message(text, tag, user);
+        /*if(file!=null){
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()) uploadDir.mkdir();
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+            message.setFilename(resultFilename);
+            file.transferTo(new File(uploadPath+"/"+resultFilename));
+        }*/
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        model.put("filter", filter);
+        return "main";
+    }
+
+    @Transactional
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+}
---
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Model model){
+        model.addAttribute("users", userRepo.findAll());
+        return "userList";
+    }
+}
